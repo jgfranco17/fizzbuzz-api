521e772218f50515dd6c10424c8019d374df2031
jgfranco17 <chino.franco@gmail.com>
2023-05-13 20:11:30 +0900

    Update: set script to run via uvicorn

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import uvicorn
 from api import load_args, create_server
 
 
 if __name__ == "__main__":
     app = create_server()
     config = load_args()
-    app.run(port=config.port)
+    uvicorn.run(app, host="0.0.0.0", port=config.port)
