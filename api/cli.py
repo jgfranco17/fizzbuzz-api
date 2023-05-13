56d9051c8d4e41043a359e716f9729a0a57c3b47
jgfranco17 <chino.franco@gmail.com>
2023-05-13 20:30:09 +0900

    Update: Added terminal header

--- api/cli.py
+++ api/cli.py
@@ -1,6 +1,7 @@
 """
 CLI interface for API.
 """
+import uvicorn
 from .utils import load_args
 from .server import create_server
 
@@ -12,7 +13,15 @@ def main():  # pragma: no cover
 
     This is the program's entry point.
     """
-    print("Launching API...")
+    header = r"""
+___________.__               __________                          _____ __________.___ 
+\_   _____/|__|______________\______   \__ __________________   /  _  \\______   \   |
+|    __)  |  \___   /\___   /|    |  _/  |  \___   /\___   /  /  /_\  \|     ___/   |
+|     \   |  |/    /  /    / |    |   \  |  //    /  /    /  /    |    \    |   |   |
+\___  /   |__/_____ \/_____ \|______  /____//_____ \/_____ \ \____|__  /____|   |___|
+    \/             \/      \/       \/            \/      \/         \/              
+    """
     app = create_server()
     config = load_args()
-    app.run(port=config.port)
+    print(header)
+    uvicorn.run(app, host="0.0.0.0", port=config.port)
