0a3258312ee50ab728c8a6a8c10504d6b50df6c1
jgfranco17 <chino.franco@gmail.com>
2023-05-15 23:42:23 +0900

    Update: added FastAPI-native exception handling

--- api/server.py
+++ api/server.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse, JSONResponse
 from .models import FizzBuzzSequence, get_data_summary
 from .computation import generate_fizzbuzz_sequence
 
@@ -17,25 +20,29 @@ def create_server() -> FastAPI:
         return {"message": "Welcome to the FizzBuzz API!"}
 
     @app.get("/fizzbuzz", status_code=200)
-    def compute(number: int = None):
+    def compute(number: Optional[int] = None):
         response = {"message": "Invalid input"}
         try:
             if number is None:
                 raise ValueError("No number provided.")
 
             if not 1 <= number <= 10**4:
-                raise ValueError("Number must a positive integer from 1 to 10^4, inclusive.")
+                raise HTTPException(status_code=404, detail="Number must a positive integer from 1 to 10^4, inclusive.")
 
             if not isinstance(number, int):
-                raise TypeError(f'Number {number} is not integer.')
-            
+                raise HTTPException(status_code=404, detail=f'Number {number} is not integer.')
+
             raw_output = generate_fizzbuzz_sequence(number)
             output_summary = FizzBuzzSequence(raw_output)
             response = get_data_summary(output_summary)
 
-        except (TypeError, ValueError) as e:
+        except HTTPException as e:
             print(f'Invalid input: {e}')
 
         return response
+
+    @app.exception_handler(RequestValidationError)
+    async def validation_exception_handler(request, exc):
+        return PlainTextResponse(str(exc), status_code=400)
 
     return app
