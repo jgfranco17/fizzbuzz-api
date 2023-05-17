471d6f83fd5216a57e493ca42c7c5fbf73d2ca0c
jgfranco17 <chino.franco@gmail.com>
2023-05-17 14:42:12 +0900

    Added function to read version number from file

--- api/cli.py
+++ api/cli.py
@@ -1,18 +1,32 @@
 """
 CLI interface for API.
 """
+import io
+import os
 import uvicorn
 from .utils import load_args
 from .server import create_server
 
 
-def main():  # pragma: no cover
+def main():
     """
     The main function executes on commands:
     `python3 -m fizzbuzz-api` and `$ fizzbuzz-api`.
 
     This is the program's entry point.
     """
+    def read(*paths, **kwargs):
+        """
+        Read the contents of a text file safely.
+        """
+        content = ""
+        with io.open(
+            os.path.join(os.path.dirname(__file__), *paths),
+            encoding=kwargs.get("encoding", "utf8"),
+        ) as file:
+            content = file.read().strip()
+        return content
+
     header = r"""
 ___________.__               __________                          _____ __________.___
 \_   _____/|__|______________\______   \____________________   /  _  \\______   \   |
@@ -23,5 +37,7 @@ ___________.__               __________                          _____ _________
     """
     app = create_server()
     config = load_args()
+    version = read("VERSION")
+    print(f'Running FizzBuzz API v{version}')
     print(header)
     uvicorn.run(app, host="0.0.0.0", port=config.port)
