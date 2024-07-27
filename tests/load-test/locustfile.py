12d77a3da4051df2aead71360f332101a67d6d30
Joaquin Franco <chino.franco@gmail.com>
2024-07-27 10:27:07 +0900

    fix: use staticmethod for logging

--- tests/load-test/locustfile.py
+++ tests/load-test/locustfile.py
@@ -1,6 +1,8 @@
 import logging
+from http import HTTPStatus
 from random import randint
 
+import requests
 from locust import HttpUser, between, task
 
 logger = logging.getLogger(__package__)
@@ -14,12 +16,28 @@ logging.basicConfig(
 class FizzbuzzUser(HttpUser):
     wait_time = between(1, 5)
 
-    @task
-    def get_fizzbuz_sequence(self):
-        number = randint(10, 25)
-        endpoint = f"/v0/fizzbuzz?number={number}"
-        response = self.client.get(endpoint)
-        if response.status_code == 200:
+    @staticmethod
+    def log_response(endpoint: str, status_code: int, response: requests.Response):
+        if response.status_code == status_code:
             logger.info(f"GET {endpoint} - success")
         else:
             logger.warning(f"GET {endpoint} - fail")
+
+    @task(10)
+    def test_get_service_info(self):
+        endpoint = f"/service-info"
+        response = self.client.get(endpoint)
+        self.log_response(endpoint, HTTPStatus.OK, response)
+
+    @task(50)
+    def test_get_fizzbuz_sequence(self):
+        number = randint(10, 25)
+        endpoint = f"/v0/fizzbuzz?number={number}"
+        response = self.client.get(endpoint)
+        self.log_response(endpoint, HTTPStatus.OK, response)
+
+    @task(1)
+    def test_not_found(self):
+        endpoint = f"/missing"
+        response = self.client.get(endpoint)
+        self.log_response(endpoint, HTTPStatus.NOT_FOUND, response)
